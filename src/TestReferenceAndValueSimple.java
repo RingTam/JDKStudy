06ddd28d11b1094f426311b2438d56d21e1c2074
Monster <807672812@qq.com>
2018-05-30 11:18:31 +0800

    值传递和引用传递。。。。。。。。。。

--- src/TestReferenceAndValueSimple.java
+++ src/TestReferenceAndValueSimple.java
@@ -12,48 +12,55 @@ public class TestReferenceAndValueSimple {
 
     public int b = 0;
 
-    public Integer[] c = new Integer[]{0, 1, 2};
+    public Integer[] c = new Integer[]{0, 0, 0};
 
     public static void main(String args[]) {
         TestReferenceAndValueSimple simple = new TestReferenceAndValueSimple();
         simple.change(simple.a, simple.b);
+
         System.out.println(simple.a);
         System.out.println(simple.b);
         System.out.println("-----------------------------------------------");
+
         simple.changeArray(simple.a, simple.c);
+
         System.out.println(Arrays.toString(simple.c));
         System.out.println("-----------------------------------------------");
+
         simple.changeObject();
+
         System.out.println(simple.a);
         System.out.println(simple.b);
         System.out.println("-----------------------------------------------");
+
+        simple.changeArrayReplace(simple.a, simple.c);
         System.out.println(Arrays.toString(simple.c));
     }
 
-    public void change(Integer a, int b) {//值传递，只修改拷贝的副本
+    public void change(Integer a, int b) {//方法参数属于值传递，只修改拷贝的副本
         //java都是采用值传递，修改无效
         a = 1;
         //java都是采用值传递，修改无效
         b = 1;
     }
 
-    public void changeArray(Integer a, Integer[] c) {//数组内存分配 栈:c --> 堆:[[0][1][2]]，相当于引用传递
+    public void changeArray(Integer a, Integer[] c) {//方法参数，如果是数组(内存分配 栈:c --> 堆:[[0][1][2]] <-- c2)，相当于拷贝引用传递
         //java都是采用值传递，修改无效
         a = 1;
-        //java都是采用值传递，但通过地址可改变里面的值
+        //拷贝引用，但可通过地址改变里面的值
         c[0] = 1;
     }
 
-    public void changeObject() {//直接修改当前对象引用的值字段。
+    public void changeObject() {//非用法参数，直接修改当前对象引用的值字段。
         //java都是采用值传递，但通过地址可改变里面的值
         this.a = 1;
         //java都是采用值传递，但通过地址可改变里面的值
         this.b = 1;
     }
 
-    public void changeArrayReplace(Integer a, Integer[] c) {//数组内存分配 栈:c --> 堆:[[0][1][2]]，相当于引用传递
+    public void changeArrayReplace(Integer a, Integer[] c) {//方法参数，如果是数组(内存分配 栈:c --> 堆:[[0][1][2]] <-- c2)，相当于拷贝引用传递
         a = 1;
-        //java都是采用值传递，修改无效
+        //堆:[[3][4][5]] --> c2，原引用指向的内容不变。
         c = new Integer[]{3, 4, 5};
     }
 }
